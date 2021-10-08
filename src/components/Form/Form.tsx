fa3ca0a57d5a081573bfbdf833773176db86c5ac
John Irle <irlejohn@gmail.com>
2021-10-08 12:05:07 -0500

    add form onSubmit and tests

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,20 +1,31 @@
-import React, { useState } from "react";
+import React, { FormEventHandler, useState } from "react";
 import styles from "./Form.module.css";
 
-const Form = () => {
+interface FormProps {
+    onSubmit: (IPorDomain: string) => void;
+}
+
+const Form = ({ onSubmit }: FormProps) => {
     const [IPorDomain, setIPorDomain] = useState("");
 
     return (
-        <form className={styles.form}>
-            <label>
-                Seach with an IP or Domain.
-                <input
-                    type="text"
-                    className={styles.input}
-                    value={IPorDomain}
-                    onChange={(e) => setIPorDomain(e.target.value)}
-                />
+        <form
+            className={styles.form}
+            onSubmit={(e) => {
+                e.preventDefault();
+                onSubmit(IPorDomain);
+            }}
+        >
+            <label htmlFor="searchInput">
+                Search whois with an IP or Domain.
             </label>
+            <input
+                type="text"
+                id="searchInput"
+                className={styles.input}
+                value={IPorDomain}
+                onChange={(e) => setIPorDomain(e.target.value)}
+            />
             <button type="submit" className={styles.submitButton}>
                 Submit
             </button>
